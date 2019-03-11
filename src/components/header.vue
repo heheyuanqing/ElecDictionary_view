9dc40653486dcb5e0566c878ad165eb7e6c1a7db
heheyuanqing <2570394541@qq.com>
2019-03-11 23:18:48 +0800

    header

---
+++ src/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <div class="logo"></div>
+    <div v-if="isSearchPage">
+      <input class="searchInput"/>
+    </div>
+    <div class="usr" v-if="isHost">
+        <button>登录/注册</button>
+    </div>
+    <div v-else>{{usrname}}</div>
+  </div>
+</template>
+<script>
+  exports.default={
+    data(){
+      usrname:"heheyuanqing"
+    }
+  }
+</script>
+<style>
+.header{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+</style>
