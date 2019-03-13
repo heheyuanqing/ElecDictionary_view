e2d883b99f30cf06503f9a73e69dcef1dbb2b3df
heyuanqing <heyuanqing@meituan.com>
2019-03-13 19:49:43 +0800

    搜索页面组件

---
+++ src/components/Search.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="main">
+    <div class="logo">LOGO</div>
+    <div class="input-part">
+      <input type="text">
+    </div>
+    <div class="btn-part">
+        <button>搜索</button>
+        <button style="margin-left:10px;">更多单词</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+.main{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.input-part,.btn-part{
+  width:100%;
+  display:flex;
+  justify-content:center;
+}
+input{
+  width: 40%;
+  height: 30px;
+  border:1px solid #dddddd;
+  border-radius: 6px;
+}
+button{
+  margin-top: 15px;
+  width: 100px;
+  height:30px;
+  border:0;
+  border-radius: 4px;
+  background-color: #ffe057;
+}
+</style>
---
+++ src/components/btn.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button>{{value}}</button>
+    </div>
+</template>
+<script>
+export default {
+    data:{
+        return {
+            value:this.props.value
+        }
+    }
+}
+</script>
+<style>
+button{
+  width: 100px;
+  height:30px;
+  border:0;
+  border-radius: 4px;
+  background-color: #ffe057;
+}
+</style>
